53c1749e75f64a9d19719ab9ac510e1511b27eae
Darwin Betancur <125227319+FalcaB@users.noreply.github.com>
2023-11-06 17:56:41 -0500

    Mini-Ajuste

--- jugador.cpp
+++ jugador.cpp
@@ -34,22 +34,23 @@ void Jugador::indicarPosicion(int &fila, int &columna, int tamano)
         if (posicion.length() != 2) {
             cout << "Entrada no valida. Debe tener exactamente una letra y un numero." << endl;
         }
-
-        // Extraer la letra y el número de la entrada
-        letra = toupper(posicion[0]);//Cambia la letra a mayúscula
-        numero = posicion[1] - '0';
-
-        // Verificar que la letra sea una letra válida (por ejemplo, 'A', 'B', 'C', ...)
-        if (letra < 'A' || letra > 'A'+ tamano){
-            cout << "Letra no valida." << endl;
-        }
-
-        // Verificar que el número sea un número válido (por ejemplo, 1, 2, 3, ...)
-        else if (numero < 1 || numero > tamano) {
-            cout << "Numero no valido. Debe estar en el rango de 1 a "<< tamano <<"." << endl;
-        }
         else{
-            break;
+            // Extraer la letra y el número de la entrada
+            letra = toupper(posicion[0]);//Cambia la letra a mayúscula
+            numero = posicion[1] - '0';
+        
+            // Verificar que la letra sea una letra válida (por ejemplo, 'A', 'B', 'C', ...)
+            if (letra < 'A' || letra > 'A'+ tamano){
+                cout << "Letra no valida." << endl;
+            }
+        
+            // Verificar que el número sea un número válido (por ejemplo, 1, 2, 3, ...)
+            else if (numero < 1 || numero > tamano) {
+                cout << "Numero no valido. Debe estar en el rango de 1 a "<< tamano <<"." << endl;
+            }
+            else{
+                break;
+            }
         }
     }
     // Calcular las coordenadas numéricas (ten en cuenta que 'A' se mapea a 0, 'B' a 1, 'C' a 2, ...)
